9fa9991c5e47895a18d99d4b76274844dd6c5d44
Artur Balabanov <arturbalabanov97@gmail.com>
2014-11-08 15:38:42 +0200

    Middle commit

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import os
 import pygame
 
 from player import Player
-#
-# class Bar(object):
-#     def __init__(self, width, height, percent, bgcolor, screen):
-#         self.width = width
-#         self.height = height
-#         self.percent = percent
-#         self.bgcolor = bgcolor
-#         self.screen = screen
-#
-#     def
+
+
+class Progressbar(object):
+    def __init__(self, width, height, percentage, bgcolor, border_color, label):
+        self.rect = pygame.Rect(200 + 1, 200 + 1, width, height)
+        self._border = pygame.Rect(200, 200, width, height)
+        self.percentage = percentage
+        self.bgcolor = bgcolor
+        self.border_color = border_color
+        self.label = label
+
+    def draw(self, screen):
+        pygame.draw.rect(screen, self.border_color, self._border, 1)
+        pygame.draw.rect(screen, self.bgcolor, self.rect)
 
 
 class Panel(object):
@@ -38,7 +42,7 @@ class Game(object):
         sprites = pygame.sprite.Group()
         self.player = Player(sprites)
 
-        self.panel = Panel((255, 0, 0), screen)
+        self.panel = Panel((255, 0, 0))
         # self.panel = pygame.Rect(self.RESOLUTION[0] - self.PANEL_WIDTH, 0,
         #                          self.PANEL_WIDTH, self.RESOLUTION[1])
 
@@ -53,6 +57,9 @@ class Game(object):
                     wall.rect = pygame.rect.Rect((x, y), block.get_size())
         sprites.add(self.walls)
 
+        self.test_progressbar = Progressbar(100, 20, 33, (0, 0, 200),
+                                            (0, 0, 0), "Test")
+
         while 1:
             dt = clock.tick(self.FPS)
 
@@ -66,6 +73,7 @@ class Game(object):
             sprites.update(dt / 1000., self)
             screen.fill((100, 100, 100))
             self.panel.draw(screen)
+            self.test_progressbar.draw(screen)
             sprites.draw(screen)
             pygame.display.flip()
 
