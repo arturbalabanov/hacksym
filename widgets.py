e86c261dbd000fdb556dbf4ce7a23c3ba54edfe9
Artur Balabanov <arturbalabanov97@gmail.com>
2014-11-08 16:04:40 +0200

    Progressbar now has persantage

--- widgets.py
+++ widgets.py
@@ -5,16 +5,19 @@ from config import Options
 
 class Progressbar(object):
     def __init__(self, width, height, percentage, bgcolor, border_color, label):
+        self.width = width
+        self.height = height
         self.rect = pygame.Rect(200, 200, width, height)
-        self._border = pygame.Rect(200 - 1, 200 - 1, width + 1, height + 1)
         self.percentage = percentage
         self.bgcolor = bgcolor
         self.border_color = border_color
         self.label = label
 
     def draw(self, screen):
-        pygame.draw.rect(screen, self.border_color, self._border, 1)
-        pygame.draw.rect(screen, self.bgcolor, self.rect)
+        res = pygame.Rect(200, 200, self.width * self.percentage / 100,
+                          self.height)
+        pygame.draw.rect(screen, self.bgcolor, res)
+        pygame.draw.rect(screen, self.border_color, self.rect, 1)
 
 
 class Panel(object):
