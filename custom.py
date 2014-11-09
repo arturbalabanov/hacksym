8a243ef7019bd2ecba3c2072a3b840e0285043ac
Nikola Jichev <njichev@gmail.com>
2014-11-09 13:39:03 +0200

    Added text in second menu

--- custom.py
+++ custom.py
@@ -20,16 +20,15 @@ class Custom(object):
         menu.y = 400
         menu.enableEffect('raise-col-padding-on-focus', enlarge_time=0.1)
 
-        rect1 = pygame.Rect(200, 25, 300, 50)
-        rect2 = pygame.Rect(50, 125, 400, 40)
-        rect3 = pygame.Rect(50, 225, 400, 40)
-        rect4 = pygame.Rect(50, 325, 400, 40)
+        rect1 = pygame.Rect(200, 25, 310, 50)
+        rect2 = pygame.Rect(50, 125, 300, 40)
+        rect3 = pygame.Rect(50, 225, 300, 40)
+        rect4 = pygame.Rect(50, 325, 300, 40)
         rect5 = pygame.Rect(500, 125, 40, 40)
         rect6 = pygame.Rect(500, 225, 40, 40)
         rect7 = pygame.Rect(500, 325, 40, 40)
-        # text = "%s: %.1f%%" % (self.label, self.percentage)
-        # screen.blit(
-        #     self.font.render(text, True, (0, 0, 0), (0, 0)), self.rect)
+
+        font = pygame.font.SysFont('Arial', 30, bold=True)
 
         while self.running:
             menu.update(pygame.event.get(), clock.tick(30)/1000.)
@@ -42,6 +41,17 @@ class Custom(object):
             pygame.draw.rect(screen, (0, 0, 255), rect5)
             pygame.draw.rect(screen, (0, 0, 255), rect6)
             pygame.draw.rect(screen, (0, 0, 255), rect7)
+            screen.blit(font.render("Customize Character",
+                                    True, (0, 0, 0), (0, 0)), rect1)
+            screen.blit(font.render("Programming",
+                                    True, (0, 0, 0), (0, 0)), rect2)
+            screen.blit(font.render("Design",
+                                    True, (0, 0, 0), (0, 0)), rect3)
+            screen.blit(font.render("Soft Skills",
+                                    True, (0, 0, 0), (0, 0)), rect4)
+            screen.blit(font.render("6", True, (0, 0, 0), (0, 0)), rect5)
+            screen.blit(font.render("1", True, (0, 0, 0), (0, 0)), rect6)
+            screen.blit(font.render("13", True, (0, 0, 0), (0, 0)), rect7)
             pygame.display.flip()
 
 if __name__ == '__main__':
